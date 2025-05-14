8133d336701f576a738526cfd15b16b4dac3e49f
drweb <drweb@DESKTOP-6245KLE>
2025-05-14 08:17:38 +0300

    добавлен импорт функции __ в вывод команды /queue

--- templates/default/controllerCommandQueue.php
+++ templates/default/controllerCommandQueue.php
@@ -8,6 +8,8 @@ use function losthost\Oberbot\ticketMention;
 use function losthost\Oberbot\ticketMentionNoId;
 use losthost\Oberbot\view\Emoji;
 
+use function \losthost\Oberbot\__;
+
 $index = 0;
 foreach ($queue as $ticket) {
     $index++;

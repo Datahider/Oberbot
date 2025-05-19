a5410db81a5caf6a083c8328f37e0fa9834261a0
drweb_000 <drweb_000@192.168.0.105>
2025-05-19 17:34:17 +0300

    Сделан код для команды /call -- можно пользоваться Сделаны правильные уведомления о срочных задачах

--- bot/controller/command/CommandCall.php
+++ bot/controller/command/CommandCall.php
@@ -2,17 +2,31 @@
 
 namespace losthost\Oberbot\controller\command;
 
+use losthost\Oberbot\data\ticket;
+use losthost\Oberbot\controller\action\ActionCreateTicket;
+
 use function \losthost\Oberbot\sendMessage;
 use function \losthost\Oberbot\__;
+use function \losthost\Oberbot\ticketMention;
+use function \losthost\Oberbot\getMentionedIds;
+use function \losthost\Oberbot\mentionByIdArray;
 
 class CommandCall extends AbstractAuthCommand {
     
     const COMMAND = 'call';
     
+    protected array $mentioned_ids;
+
+
     protected function handle(\TelegramBot\Api\Types\Message &$message): bool {
     
-        sendMessage(__('Не реализовано'), null, $this->chat_id, $this->thread_id);
+        $this->mentioned_ids = getMentionedIds($message);
         
+        if ($this->thread_id >= 2) {
+            $this->createAndStartConsultSubtask();
+        } else {
+            $this->createAndStartConsultTask();
+        }
         return true;
     }
 
@@ -24,6 +38,37 @@ class CommandCall extends AbstractAuthCommand {
         return [
             'default' => 'Создание (под)задачи с типом Консультация'
         ];
+    }
+    
+    protected function createAndStartConsultSubtask() {
+        $old_ticket = ticket::getByGroupThread($this->chat_id, $this->thread_id);
+        
+        if (empty($this->mentioned_ids)) {
+            $this->mentioned_ids = $old_ticket->getCustomers();
+        }
+        $mention = mentionByIdArray($this->mentioned_ids);
+        $mention4title = $this->getMentions4Title($mention);
+        
+        $ticket = ActionCreateTicket::do($this->chat_id, $this->chat_id, $this->user_id, __('Консультация %user%', ['user' => $mention4title]), __('Согласованная консультация по задаче %ticket%', ['ticket' => ticketMention($old_ticket)]));
+        $ticket->setType(ticket::TYPE_SCHEDULED_CONSULT);
+        $old_ticket->waitTask($ticket->id);
+        $ticket->timerStart($this->user_id);
+    }
+
+    protected function createAndStartConsultTask() {
+        if (empty($this->mentioned_ids)) {
+            sendMessage(__('Вы не указали пользователя, от которого получен звонок.'));
+        } else {
+            $mentions = mentionByIdArray($this->mentioned_ids);
+            $mentions4title = $this->getMentions4Title($mentions);
+            $ticket = ActionCreateTicket::do($this->chat_id, $this->chat_id, $this->user_id, __('Срочная консультация %user%', ['user' => $mentions4title]), __('Срочная консультация пользователя %mention%', ['mention' => $mentions]));
+            $ticket->setType(ticket::TYPE_URGENT_CONSULT);
+            $ticket->timerStart($this->user_id);
+        }
+    }
+    
+    protected function getMentions4Title($mentions) {
+        return preg_replace("/\<.*?\>/", '', $mentions);
     }
 
 }
--- templates/default/privateUrgentNotification.php
+++ templates/default/privateUrgentNotification.php
@@ -1,12 +1,23 @@
 <?php
 
 use losthost\Oberbot\view\Emoji;
+use losthost\Oberbot\data\ticket;
+use function losthost\Oberbot\__;
+use function \losthost\Oberbot\ticketMention;
 
 $url = str_replace('-100', 'c/', "https://t.me/$ticket->chat_id/$ticket->topic_id");
 
-if ($ticket->is_task) {
-    echo Emoji::TASK_PRIORITY. " Внимание! Срочная заявка: <a href=\"$url\">$ticket->title - #$ticket->id</a>";
+$icon = Emoji::TEXT_EMOJI_BY_TYPE[$ticket->type];
+$type_text = lcfirst(__('type_'.$ticket->type));
+$ticket_mention = ticketMention($ticket);
+
+if ($ticket->status == ticket::STATUS_NEW) {
+    echo "$icon Новая $type_text: $ticket_mention";
+} elseif ($ticket->status == ticket::STATUS_USER_ANSWERED) {
+    echo "$icon Пользователь ответил в $ticket_mention";
+} elseif ($ticket->status == ticket::STATUS_REOPEN) {
+    echo "$icon Переоткрыта $type_text $ticket_mention";
 } else {
-    echo Emoji::ICON_EXCLAMATION. " Возникла неисправность: <a href=\"$url\">$ticket->title - #$ticket->id</a>";
+    echo "Глюк уведомления: $ticket_mention";
 }
 

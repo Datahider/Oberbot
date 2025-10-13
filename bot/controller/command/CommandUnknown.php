b4d7345db9b9e052663df66878082fd4e11e6d5c
drweb_000 <drweb_000@192.168.0.105>
2025-10-13 11:06:56 +0300

    Сделал, чтобы CommandUnknown не обрабатывал неизвестные команды состоящие только из цифр. Т.к. обработка цифр делается позже

--- bot/controller/command/CommandUnknown.php
+++ bot/controller/command/CommandUnknown.php
@@ -5,7 +5,8 @@ namespace losthost\Oberbot\controller\command;
 class CommandUnknown extends CommandNote {
     
     protected function check(\TelegramBot\Api\Types\Message &$message): bool {
-        if (preg_match("/^\//", $message->getText())) {  // Обрабатывает любую строку, начинающуюся с / и не обработанную до этого 
+        if (preg_match("/^\/(?!\d+(\s|$))/", $message->getText())) {  // Обрабатывает любую строку, начинающуюся с / и не обработанную до этого 
+                                                         // если только это не команда состоящая только из цифр. Т.к. тогда она будет обработана позже       
             return true;                                 // Чтобы бот не реагировал на непонятные команды в группах, относящиеся к другим ботам
         }
         return false;
